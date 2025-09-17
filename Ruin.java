4846fe85bd3c5e62a4d4fb04b6a7562cc0c1bf4f
Robin Koehler <koehler@wustl.edu>
2025-09-17 12:09:41 -0700

    Studio 3

---
+++ Ruin.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ruin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Till what number should it find primes?");
+        int n = scanner.nextInt();
+        int[] arrayLength = new int[n];
+        for (int i = 0; i < arrayLength.length; i++) { 
+            if(int i = j; j++);
+            System.out.println(arrayLength[j]); 
+            int
+        }
+            
+        scanner.close();
+    }
+}
